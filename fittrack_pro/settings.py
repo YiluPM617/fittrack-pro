54fb902763d78a49eb89a631bbe9ccf1962edeaf
Wentao Yang <ywt625@163.com>
2026-03-14 17:09:33 +0000

    Add static root for appliku deployment

--- fittrack_pro/settings.py
+++ fittrack_pro/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = 'dashboard:home'
